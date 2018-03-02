409df548c151078f0dc531eadea62e4d74b65dc4
Thomas Yoho <TYohoJr@Gmail.com>
2018-03-01 17:44:35 -0700

    work from 3-1-18

--- express.js
+++ express.js
@@ -3,28 +3,31 @@ var bodyParser = require('body-parser');
 var express = require("express");
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-app.use(bodyParser.json({type:'application/json'}));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 var db;
 
 MongoClient.connect("mongodb://localhost:27017/moby", (err, client) => {
-  if (err) return console.log(err)
-  db = client.db("moby") // whatever your database name is
-  app.listen(8080, () => {
-    console.log("listening on 8080")
-  })
+    if (err) return console.log(err)
+    db = client.db("moby") // whatever your database name is
+    app.listen(8080, () => {
+        console.log("listening on 8080")
+    })
 })
 
-app.post("/signInData", (req, res)=>{
-   db.collection("users").find({username: req.body.username}).toArray((err, user)=>{
-        if (user[0].password === req.body.password){
-            res.json(`Login successful!`); 
-            console.log("Log in sucess")
-        } else if (err){
+app.post("/signInData", (req, res) => {
+    db.collection("users").find({ username: req.body.username }).toArray((err, user) => {
+        if (!user.length) {
             res.json("Login unsuccessfull, dip-shit.");
-        } else if (!user.length){
+        } else if (err) {
             res.json("Login unsuccessfull, dip-shit.");
+        } else if (user[0].password === req.body.password) {
+            res.json(`Login successful!`);
+            console.log("Log in success")
         } else {
             console.log('You fucked up.')
             res.json("Login unsuccessfull, dip-shit.");
@@ -33,12 +36,33 @@ app.post("/signInData", (req, res)=>{
 });
 
 app.post('/signUpData', (req, res) => {
-    db.collection('users').save(req.body, (err, result) => {
-      if (err) return console.log(err);
-      console.log('saved to database');
-    });
-  });
 
-app.get("/testing", (req,res)=>{
-   res.sendfile('testing.html');
+    if (req.body.username.length && req.body.password.length) {
+        db.collection('users').find({ username: req.body.username }).toArray((err, dataMatch) => {
+            if (!dataMatch.length) {
+                bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+                    // Store hash in your password DB.
+                    db.collection('users').save({username: req.body.username, password: hash}, (err, result) => {
+                        if (err) {
+                            res.json("Failed")
+                            return console.log(err);
+                        } else {
+                            res.json("Sign Up Successful")
+                            console.log('saved to database');
+                        }
+                    });
+
+                });
+            } else {
+                res.json('This username already exists...asshole.')
+            }
+        })
+    } else {
+        res.json('Error: username or password can\'t be blank, toolbag')
+    }
+});
+
+
+app.get("/testing", (req, res) => {
+    res.sendfile('testing.html');
 });
