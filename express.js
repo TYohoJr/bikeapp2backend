fe5271f8be1612a1552b662f451a6607f5fa78be
Thomas Yoho <TYohoJr@Gmail.com>
2018-03-06 11:44:49 -0700

    commited to heroku branch

--- express.js
+++ express.js
@@ -5,49 +5,77 @@ var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var bcrypt = require('bcrypt');
 const saltRounds = 10;
+var jwt = require('jsonwebtoken');
+require('dotenv').config();
 
+app.use(express.static("build"));
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var db;
+// var decoded = jwt.verify(token, 'Secret');
 
-MongoClient.connect("mongodb://localhost:27017/moby", (err, client) => {
+function verifyToken(req, res, next) {
+    var token = req.body.token;
+    if (token) {
+        jwt.verify(token, "Secret", (err, decode) => {
+            if (err) {
+                res.send("Wrong token")
+            } else {
+                res.locals.decode = decode
+                next();
+            }
+        })
+    } else {
+        res.send("No token")
+    }
+}
+
+MongoClient.connect(`mongodb://TYohoJr:${process.env.DB_PASS}@ds257838.mlab.com:57838/bike_app`, (err, client) => {
     if (err) return console.log(err)
-    db = client.db("moby") // whatever your database name is
+    db = client.db("bike_app") // whatever your database name is
     app.listen(8080, () => {
         console.log("listening on 8080")
     })
 })
 
+app.get("/", (req, res) =>{
+    res.sendFile("index.html")
+})
+
 app.post("/signInData", (req, res) => {
     db.collection("users").find({ username: req.body.username }).toArray((err, user) => {
-        
-        if (!user.length) {
-            res.json("Login unsuccessfull, dip-shit.");
-        } else if (err) {
-            res.json("Login unsuccessfull, dip-shit.");
-        } 
 
-        bcrypt.compare(req.body.password, user[0].password, function(err, resolve) {
-            //res == true
-            console.log(resolve)
-         if (resolve === true) {
-            res.json(`Login successful!`);
-            console.log("Log in success")
-        } else {
-            console.log('You fucked up.')
-            res.json("Login unsuccessfull, dip-shit.");
+        if (!user.length) {
+            res.json("Login unsuccessfull");
+        } else if (err) {
+            res.json("Login unsuccessfull");
         }
+        bcrypt.compare(req.body.password, user[0].password, function (err, resolve) {
+            //res == true
+            if (resolve === true) {
+               var token = jwt.sign(req.body.username, ('Secret'), {
+                    //expiresInMinutes: 1440 // expires in 24 hours, no longer valid, probs deprecated
+                });
+                res.json({
+                    message: "Login successful!",
+                    myToken: token
+                });
+            } else if(resolve === false){
+                res.json({
+                    message: "Login failed!",
+                })
+            }
         });
     })
 });
-    app.post('/signUpData', (req, res) => {
+app.post('/signUpData', (req, res) => {
     if (req.body.username.length && req.body.password.length) {
         db.collection('users').find({ username: req.body.username }).toArray((err, dataMatch) => {
             if (!dataMatch.length) {
                 bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
                     // Store hash in your password DB.
-                    db.collection('users').save({username: req.body.username, password: hash}, (err, result) => {
+                    db.collection('users').save({ username: req.body.username, password: hash, work: req.body.work }, (err, result) => {
                         if (err) {
                             res.json("Failed")
                             return console.log(err);
@@ -58,10 +86,22 @@ app.post("/signInData", (req, res) => {
                     });
                 });
             } else {
-                res.json('This username already exists...asshole.')
+                res.json('This username already exists')
             }
         })
     } else {
-        res.json('Error: username or password can\'t be blank, toolbag')
+        res.json('Error: username or password can\'t be blank')
     }
 });
+
+app.post('/findRoute', verifyToken, (req, res) => {
+  // console.log(decode);
+  
+    //var decoded = jwt.decode(req.body.token, { complete: true });
+   // console.log(decoded.header);
+  //  console.log(decoded.payload)
+         db.collection('users').find({ username: res.locals.decode }).toArray((err, dataMatch) => {
+             res.json(dataMatch)
+            });
+    //  res.json()
+})
